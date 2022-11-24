03003c6a98119ae5a9db44d9020cf6c4e11cdbae
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-24 17:43:38 +0900

    5.3 Custom Admin

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,10 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        ("Profile", {"fields": ("name", "phone", "mobile_carrier", "resident_registration_number", "phone_verified")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined"), "classes": ("collapse",)}),
+    )
+
+    list_display = ("username", "name", "phone", "mobile_carrier", "resident_registration_number", "phone_verified", "is_staff")
