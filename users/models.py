b69389723ceaa2dbfcc6fd6a2cbd0f213bacedb9
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-24 17:40:57 +0900

    5.2 Custom Fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=20, blank=True)
+    mobile_carrier = models.CharField(max_length=20, blank=True)
+    resident_registration_number = models.CharField(max_length=20, blank=True)
+    name = models.CharField(max_length=20, blank=True)
+    phone_verified = models.BooleanField(default=False)
