9ea688a26511aedaa25b125d19d7dc84b8f9a74b
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-25 09:43:35 +0900

    6.0 User Model

--- users/admin.py
+++ users/admin.py
@@ -6,9 +6,9 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        ("Profile", {"fields": ("name", "phone", "mobile_carrier", "resident_registration_number", "phone_verified")}),
+        ("Profile", {"fields": ("name", "phone_number", "mobile_carrier", "resident_registration_number", "is_phone_number_verified")}),
         ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
         ("Important dates", {"fields": ("last_login", "date_joined"), "classes": ("collapse",)}),
     )
 
-    list_display = ("username", "name", "phone", "mobile_carrier", "resident_registration_number", "phone_verified", "is_staff")
+    list_display = ("username", "name", "phone_number", "mobile_carrier", "resident_registration_number", "is_phone_number_verified", "is_staff")
--- users/models.py
+++ users/models.py
@@ -3,8 +3,21 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True)
-    mobile_carrier = models.CharField(max_length=20, blank=True)
+    class MobileCarrierChoices(models.TextChoices):
+        SKT = ("SKT", "SKT")
+        KT = ("KT", "KT")
+        LGU = ("LGU", "LGU")
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    phone_number = models.CharField(max_length=20, blank=True)
+    mobile_carrier = models.CharField(max_length=3, choices=MobileCarrierChoices.choices, blank=True)
     resident_registration_number = models.CharField(max_length=20, blank=True)
+    gender = models.CharField(max_length=6, choices=GenderChoices.choices, blank=True)
     name = models.CharField(max_length=20, blank=True)
-    phone_verified = models.BooleanField(default=False)
+    is_phone_number_verified = models.BooleanField(default=False)
+    avatar = models.FileField(blank=True)
+    # address
+    # email
